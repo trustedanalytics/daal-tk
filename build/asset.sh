d5a41c79fcbf0d5cff256ca13ce8498bdd30d6a1
rodorad <rene.o.dorado@intel.com>
2016-11-30 15:45:13 -0800

    upload file not folder

--- build/asset.sh
+++ build/asset.sh
@@ -18,7 +18,7 @@
 
 JAVA_PATH=$(find $BASE_DIR/$SOURCE_DIR/daaltk-core/ -name "daaltk-core*.zip")
 PIP_PATH=$(find $BASE_DIR/$SOURCE_DIR/python/ -name "daaltk-*.tar.gz")
-DAAL_INSTALL=$(find $BASE_DIR/$SOURCE_DIR/ -name "daal-install")
+DAAL_INSTALL=$(find $BASE_DIR/$SOURCE_DIR/daal-install/ -name "daal-install")
 LICENSES_PATH=$(find `pwd` -name "licenses*.zip")
 
 echo java_path $JAVA_PATH
